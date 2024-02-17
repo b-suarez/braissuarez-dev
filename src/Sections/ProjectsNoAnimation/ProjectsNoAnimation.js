30512b0d02bdd8e33c67d8b3ce78a6065e3c6b4e
Brais Suarez <bsuarezsouto@gmail.com>
2024-02-17 22:42:37 +0100

    Updated favi on

--- src/Sections/ProjectsNoAnimation/ProjectsNoAnimation.js
+++ src/Sections/ProjectsNoAnimation/ProjectsNoAnimation.js
@@ -4,15 +4,15 @@ import { Grid } from "@mui/material";
 import SectionHeader from "../../Components/SectionHeader/SectionHeader";
 
 const projects = {
-	"Technical Translator":
+	"TECHNICAL SAVVY":
 		"My hands-on experience at every stage of software development equips me well to lead digital strategies " +
 		"and transformations. This means I can dive deep into any aspect of a project while also seeing the big picture " +
 		"from above.",
-	"Digital Expert":
+	"DIGITAL EXPERT":
 		"Throughout my career, I've completed a variety of projects ranging from airport installations to barbecue " +
 		"software, and including OTT and sports data platforms. This diverse experience has provided me with a " +
 		"comprehensive understanding of what the market offers and what's achievable.",
-	"Master of None":
+	"JACK OF ALL TRADES":
 		"I hold certification as a Google Cloud engineer and have worked as a Data Scientist, though my " +
 		"background is in game design and development. I've spent the majority of " +
 		"my career in managerial roles. This unique blend of technical expertise and managerial experience " +
@@ -30,7 +30,7 @@ class ProjectsNoAnimation extends Component {
 					{/* LOOP TO CREATE THE CARDS */}
 					{Object.keys(projects).map((key, index) => {
 						return (
-							<Grid item xs={12} md={tabWidth} className="container-tab">
+							<Grid key={key} item xs={12} md={tabWidth} className="container-tab">
 								<Grid container direction="column" justifyContent="end">
 									<Grid item xs={4}>
 										<p className="project-number">{"0" + (index + 1)}</p>
